2b68c1c2ff2d75f68af433822b710434be69db88
ahumyck <ahumyckbf4@gmail.com>
2019-11-19 15:39:49 +0400

    sub button added to navigation panel

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,8 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { AuthenticationService } from "../../service/authorization.service";
 import {ActivatedRoute, Router} from "@angular/router";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {UserService} from "../../service/user.service";
 
 @Component({
     selector: 'app-navigation',
@@ -12,17 +10,10 @@ import {UserService} from "../../service/user.service";
 export class NavigationComponent implements OnInit {
 
     currentUser: any;
-    usernameFormGroup: FormGroup;
 
     constructor(private authService: AuthenticationService,
                 private activatedRoute: ActivatedRoute,
-                private userService: UserService,
-                private formBuilder: FormBuilder,
-                private router: Router)
-    {
-        this.usernameFormGroup = this.formBuilder.group({
-            username:  ['', Validators.required],
-        });
+                private router: Router){ 
     }
 
     ngOnInit() {
@@ -33,16 +24,5 @@ export class NavigationComponent implements OnInit {
     logout() {
         this.authService.logout();
         this.router.navigate(['/sign-in'])
-    }
-
-    onSubmit() {
-        //todo: ask database if such user exists
-
-        // let answer;
-        // this.userService.ifUserExists(this.usernameFormGroup.value.username).subscribe( res => answer);
-        // if(answer) {
-        // }
-        this.router.navigate(['/posts', this.usernameFormGroup.value.username]);
-
     }
 }
